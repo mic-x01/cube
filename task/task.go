18f8bbbec4b47eab1664ac5ed9a603d0ed8b4f38
mic_x01 <m.marques.dev@gmail.com>
2024-05-10 23:09:30 -0300

    chore: start project

---
+++ task/task.go
@@ -0,0 +1,26 @@
+package task
+
+import (
+	"github.com/google/uuid"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	PortBindings  map[string]string
+	RestartPolicy string
+}
